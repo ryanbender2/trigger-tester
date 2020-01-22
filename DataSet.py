8396efc4176e943f899b50789c25b8e2ae6e7de3
ryanbender2 <52826517+ryanbender2@users.noreply.github.com>
2020-01-22 16:53:40 -0500

    DataSet: added methods and fixes

--- DataSet.py
+++ DataSet.py
@@ -1,17 +1,14 @@
-"""Return a dictionary of parsed dataset
+"""DataSet.py: Functions as an interface for bank records.
 
 Parameters:
 csv_file (str): filepath to dataset
-
-Returns:
-dict: parsed dataset
-
 """
 
 import csv
+from collections import OrderedDict
 
 
-class Dataset(object):
+class DataSet(object):
     """
     Dataset parser
     
@@ -19,22 +16,93 @@ class Dataset(object):
     :type csv_file: str
     
     """
-    
 
     def __init__(self, csv_file):
         self.csv_file = csv_file
+        self.data = self.__getdata__()
     
-    
-    def parse(self):
+    def __getdata__(self):
         data = []  # Data from csv file, each row in csv file is an OrderedDict
         
         # Parse csv file
         csvFile = open(self.csv_file, newline='')
         parsedFile = csv.DictReader(csvFile)
         for row in parsedFile:
-            data.append(row)
+            strippedRow = OrderedDict(row)  # Cleaned keys
+            keys = row.keys()
+            for i in keys:  # runs over all keys to strip
+                strippedStr = i.strip('ï»¿')
+                value = row.get(i)
+                strippedRow.pop(i)
+                strippedRow[strippedStr] = value
+            data.append(strippedRow)
 
+        return data
+    
+    def getAllRecords(self):
+        """getAllRecords: returns all records in dataset
+            
+        :params None
+        :returns data: list
+        """
+        return self.data
 
-# test
-d = Dataset('data_files\Transactions_for_IACT_Accts.csv')
-d.parse()
+    def getRecordByIndex(self, index):
+        """getRecordByIndex: returns record at specified index
+        
+        :param index: int
+        :returns record: OrderedDict
+        """
+        return self.data[index]
+    
+    def getAllValuesInCategory(self, category):
+        """getAllValuesInCategory: returns all values in specifed category
+        
+        :param category: str
+        :returns values: list
+        """
+        catExists = False
+        
+        if category in list(self.data[0].keys()):
+            catExists = True
+        
+        if catExists:
+            values = []
+            for row in self.data:
+                value = row.get(category)
+                values.append(value)
+                
+            return values
+        else:
+            return '[getAllValuesInCategory] Error: category does not exist in dataset'
+        
+    def getCategories(self):
+        """getCategories: returns all categories in dataset
+        
+        :returns categories: list
+        """
+        categories = []
+        if len(self.data) is not 0:
+            for i in self.data[0].keys():
+                categories.append(i)
+            
+            return categories
+        else:
+            return '[getCategories] Error: no records in dataset'
+    
+    def printRecord(self, index):
+        """printRecord: prints record by index, formatted
+        
+        :param index: int
+        """
+        if len(self.data) is 0:
+            return '[printRecord] Error: no records in dataset'
+        
+        string = ''
+        categories = self.getCategories()
+        values = list(self.data[index].values())
+        
+        recordNum = '\nRecord Number ' + str(index) + '\n'
+
+ 
+        return string
---
+++ DataSetTest.py
@@ -0,0 +1,16 @@
+"""
+    Testing module for the DataSet class.
+"""
+
+import random
+import DataSet
+
+test = DataSet.DataSet('data_files/test.csv')
+
+rdmIndex = random.randint(1, 9000)
+
+print('==========')
+print('test.printRecord(rdmIndex)')
+print('Random record pulled: record #' + str(rdmIndex) + '\n')
+print(test.printRecord(rdmIndex))
+print('\n==========')
